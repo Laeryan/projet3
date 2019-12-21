2d4b19e5dcbf5dadb72422c857875ef86c632b50
MrWazzat <raphael.marzat@outlook.com>
2019-12-21 19:44:27 +0100

    Création des stations et affichage onclick dans console

--- js/map.js
+++ js/map.js
@@ -1,8 +1,8 @@
-class myMap {
+class Map {
     constructor() {
         this.apiUrl = 'https://api.jcdecaux.com/vls/v1/stations?contract=Mulhouse&apiKey=508a0da7d19086274962c418f8cc5b7338052869';
         this.map = L.map('map');
-        this.tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             id: 'mapbox/streets-v11',
             accessToken: 'pk.eyJ1IjoibGFlcnlhbiIsImEiOiJjazRjbjdzdm8wcGFsM2VrZDJidWtnNTZtIn0.F0pgTBxrtqMJu58YhQgC8w'
@@ -10,26 +10,32 @@ class myMap {
         this.latitude = 47.75;
         this.longitude = 7.33;
         this.zoom = 15;
+
+        this.map.setView([47.74329631548472, 7.31549439585265], 13);
+        
+        this.createStationsOnMapFromJSON(this.map, this.apiUrl);
+
+
+    }
+
+    createStationsOnMapFromJSON(map, apiUrl){
+        var request = new XMLHttpRequest()
+        request.open('GET', this.apiUrl, false);
+
+        request.onload = function(){
+            var data = JSON.parse(this.response);
+            
+            data.forEach(element => {
+                console.log(element.position.lng);
+                var station = new Station()
+                station.createMarker(map, element.position.lat, element.position.lng);
+            });
+        }
+
+        request.send();
     }
     
 }
-
-/*
-var myMap = L.map('map').setView([47.75, 7.33], 13);
-
-).addTo(myMap);
-
-const apiUrl = 'https://api.jcdecaux.com/vls/v1/stations?contract=Mulhouse&apiKey=508a0da7d19086274962c418f8cc5b7338052869';
-
-
-request.open('GET','https://api.jcdecaux.com/vls/v1/stations?contract=Mulhouse&apiKey=508a0da7d19086274962c418f8cc5b7338052869');
-request.send(null);
-
-
-L.marker([latitude, longitude]).addTo(myMap);
-
-
-
 
 class Station {
     constructor(number, name, position, status, address, totalStands) {
@@ -39,25 +45,23 @@ class Station {
         this.status = status;
         this.address = address;
         this.places = totalStands;
-
-    addMarker() {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        const {latitude, longitude} = data;
     }
+
+    createMarker(map, latitude, longitude){
+        this.marker = L.marker([latitude, longitude]).addTo(map);
+        this.marker.on('click', this.test);
+
+    }
+
+    test(){
+        console.log("Yo");
+    }
+    
+
 }
 
 
+const myMap = new Map()
 
-var requestApiJcDecaux = new Request("GET",
-    "https://api.jcdecaux.com/vls/v1/stations?contract=Mulhouse&apiKey=508a0da7d19086274962c418f8cc5b7338052869");
 
-requestApiJcDecaux.call(function (response) {
 
-    response = JSON.parse(response);
-    response.forEach(function (info) {
-        var latitude = info.position.lat;
-        var longitude = info.position.lng;
-        L.marker([latitude, longitude]).on('click', onMarkerClick).addTo(myMap);
-    });
-    */
